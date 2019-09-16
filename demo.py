15db8c8faefc75106aa672e5d8501ac4e6121551
Collin_Stubbs <collin.stubbs@greenwoodcollege.com>
2019-09-16 11:30:43 -0400

    Functions review quick demo

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
-file = open("examplefile.txt", "a")
-file.write("add to end")
-file.close()
-file = open("examplefile.txt", "r")
-print(file.read())
+def printLoop(loop_amount, message):
+    for i in range(loop_amount):
+        print(message)
+
+num = 3
+message = "Hello World!"
+
+printLoop(num, message)
