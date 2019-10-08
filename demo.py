2bcab896d8226d4fd7cd02399af67f1db7e47e80
Collin_Stubbs <collin.stubbs@greenwoodcollege.com>
2019-10-08 15:57:10 -0400

    Dictionary Examples

--- demo.py
+++ demo.py
@@ -1,28 +1,6 @@
-import random
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-grid = []
-length = 7
-
-for i in range(length):
-    grid.append([])
-    for j in range(length):
-        letter = random.randint(0,25)
-        grid[i].append(alphabet[letter])
-
-    print(grid[i])
-
-listWords = ["word1", "word2", "word3", "word4", "word5", "word6"]
-newList = []
-for i in listWords:
-    randomI = random.randint(0, 5)
-    while listWords[randomI] in newList:
-        randomI = random.randint(0, 5)
-    newList.append(listWords[randomI])
-
-print(newList)
-
-
-
-
+school_rooms = {174: "cotaught class and break out rooms", 230: "small breakoutroom, warm", 412:"fourth floor lab"}
+ex_dictionary = {True: "yes", False: "No"}
+print(school_rooms.keys())
+test = school_rooms.keys()
+for key in test:
+    del school_rooms[key]
