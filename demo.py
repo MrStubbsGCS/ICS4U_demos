48f040f3f1e103eaca9cd23f5ddae59018e101a1
Collin_Stubbs <collin.stubbs@greenwoodcollege.com>
2019-10-13 11:06:08 -0400

    Dictionary Loops

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
-school_rooms = {174: "cotaught class and break out rooms", 230: "small breakoutroom, warm", 412:"fourth floor lab"}
-ex_dictionary = {True: "yes", False: "No"}
-print(school_rooms.keys())
-test = school_rooms.keys()
-for key in test:
-    del school_rooms[key]
+dict = {}
+
+for i in range(35):
+    dict[i] = str(i)
+
+for x in dict:
+    if dict[x] == "33":
+        print(x)
